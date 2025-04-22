3bcdc3f734be0866dc51426e97066a78731c2bff
georgi <Georgi_Ivanov92@pm.me>
2025-04-22 18:23:09 +0200

    init commit

---
+++ src/main/java/org/rtm/RustTeamManagerApplication.java
@@ -0,0 +1,13 @@
+package org.rtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustTeamManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RustTeamManagerApplication.class, args);
+    }
+
+}
