73a77bbf8e2a4cefbb3dc11ba5e00375b8168d42
georgi <Georgi_Ivanov92@pm.me>
2025-04-23 17:37:27 +0200

    Created enums for Sleeve Condition

--- src/main/java/org/rtm/model/entity/Sleeve.java
+++ src/main/java/org/rtm/model/entity/Sleeve.java
@@ -1,11 +1,15 @@
 package org.rtm.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.rtm.model.enums.SleeveCondition;
+import org.rtm.model.enums.SleeveType;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
@@ -25,6 +29,7 @@ public class Sleeve extends BaseEntity {
     private String manufacturer;
     private String notes;
     private LocalDate manufactureDate;
+    private Long width;
 
     @LastModifiedDate
     private LocalDate lastUsedAt;
@@ -34,4 +39,9 @@ public class Sleeve extends BaseEntity {
     // Warehouse
 
     private String status;
+
+    @Enumerated(EnumType.STRING)
+    private SleeveType type;
+
+    private SleeveCondition condition;
 }
