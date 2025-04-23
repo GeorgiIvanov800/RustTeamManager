79224a95b3a78d559616a089ddcde5cd09277261
georgi <Georgi_Ivanov92@pm.me>
2025-04-23 17:19:49 +0200

    Created Sleeve Entity

---
+++ src/main/java/org/rtm/model/entity/Sleeve.java
@@ -0,0 +1,38 @@
+package org.rtm.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "sleeves")
+public class Sleeve extends BaseEntity {
+
+    private String sequenceNumber; // Reference number (Satznummer)
+    private String design;
+    private String color;
+    private String manufacturer;
+    private String notes;
+
+    private LocalDate manufactureDate;
+
+    @LastModifiedDate
+    private LocalDate lastUsedAt;
+
+    private Long kmStand;
+
+    // Warehouse
+
+    private String status;
+}
