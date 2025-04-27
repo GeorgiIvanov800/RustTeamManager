721fb260959580978f0784652694f776d96f2aff
georgi <Georgi_Ivanov92@pm.me>
2025-04-27 22:33:22 +0200

    create mapper interface

--- src/main/java/org/rtm/mapper/SleeveMapper.java
+++ src/main/java/org/rtm/mapper/SleeveMapper.java
@@ -1,7 +1,14 @@
 package org.rtm.mapper;
 
 import org.mapstruct.Mapper;
+import org.rtm.model.dto.request.SaveSleeveRequest;
+import org.rtm.model.dto.response.SaveSleeveResponse;
+import org.rtm.model.entity.Sleeve;
 
 @Mapper(componentModel = "spring")
 public interface SleeveMapper {
+
+    Sleeve toEntity(SaveSleeveRequest request);
+
+    SaveSleeveResponse toResponse(Sleeve sleeve);
 }
